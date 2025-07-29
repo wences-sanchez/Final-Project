f484590a7b9e47e939132868ce92f0d6c2d77a69
Wenceslao Sánchez Pino <wences.tuning@gmail.com>
2025-07-29 21:03:38 +0200

    Renamed branch value

--- get_new_url.sh
+++ get_new_url.sh
@@ -1,3 +1,3 @@
 AWS_APP_ID=$(aws amplify list-apps --query "apps[?name=='Final-Project'].appId" --output text)
-AWS_BRANCH='tfm'
+AWS_BRANCH='main'
 aws amplify get-branch --app-id $AWS_APP_ID --branch-name $AWS_BRANCH --query "branch.webUrl" --output text
